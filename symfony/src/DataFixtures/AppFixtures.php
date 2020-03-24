bca299a0eac7b7b574c0cc4a4e767ed4f2aae28f
ASYOU99 <asyou1985@gmail.com>
2020-03-25 00:13:36 +0200

    add users entity

--- symfony/src/DataFixtures/AppFixtures.php
+++ symfony/src/DataFixtures/AppFixtures.php
@@ -2,16 +2,47 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->loadUsers($manager);
+    }
+
+    private function loadUsers(ObjectManager $manager): void
+    {
+        foreach ($this->getUserData() as [$fullname, $password, $email, $roles]) {
+            $user = new User();
+            $user->setFullName($fullname);
+            $user->setPassword($this->passwordEncoder->encodePassword($user, $password));
+            $user->setEmail($email);
+            $user->setRoles($roles);
+
+            $manager->persist($user);
+            $this->addReference($email, $user);
+        }
 
         $manager->flush();
+    }
+
+    private function getUserData(): array
+    {
+        return [
+            // $userData = [$fullname, $password, $email, $roles];
+            ['ADMIN', 'admin', 'admin@admin.com', ['ROLE_ADMIN']],
+        ];
     }
 }
